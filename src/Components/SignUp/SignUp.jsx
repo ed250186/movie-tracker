a00b1dd3c73153bb5bc42228b56a491ebeba29ba
Nathan <nathan.froeh@gmail.com>
2019-07-17 13:07:33 -0600

    Add handleChange

--- src/Components/SignUp/SignUp.jsx
+++ src/Components/SignUp/SignUp.jsx
@@ -9,13 +9,38 @@ class SignUp extends Component {
       password: ''
     }
   }
-  
+
   url = 'http://localhost:3000/api/users/new'
   // const post = 
+
+  handleChange = (e) => {
+    const {name, value} = e.target;
+    this.setState({[name]: value})
+  }
   return() {
     return (
       <div>
-
+        <form action="">
+          <input 
+            type="text" 
+            placeholder='name' 
+            name='name'
+            onChange={this.handleChange}
+          />
+          <input 
+            type="text" 
+            placeholder='email' 
+            name='email'
+            onChange={this.handleChange}
+          />
+          <input 
+            type="text" 
+            placeholder='password' 
+            name='password'
+            onChange={this.handleChange}
+          />
+          <input type="submit" name="" id=""/>
+        </form>
       </div>
     )
   }
