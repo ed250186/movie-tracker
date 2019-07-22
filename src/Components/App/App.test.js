4837f9437ea3f581edf0f0dba75d08a0fa3175ff
Nathan <nathan.froeh@gmail.com>
2019-07-22 14:25:25 -0600

    Add App Snapshot

---
+++ src/Components/App/App.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import App from './App';
+
+describe('App', () => {
+  let wrapper, instance;
+
+  beforeEach(() => {
+    wrapper = shallow(<App/>);
+    instance = wrapper.instance();
+  })
+
+  it('should match snapshot', () => {
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  
+
+})
