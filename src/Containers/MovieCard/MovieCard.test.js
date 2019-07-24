966752af429bd0f5f6092360de52cc776e17890c
Nathan <nathan.froeh@gmail.com>
2019-07-23 21:21:35 -0600

    MovieCard error state test

--- src/Containers/MovieCard/MovieCard.test.js
+++ src/Containers/MovieCard/MovieCard.test.js
@@ -7,15 +7,20 @@ import {MovieCard} from './MovieCard';
 
 //lines   ... 44,161,162,163
 describe('App', () => {
-  let wrapper
+  let wrapper, instance
   let mockFunc = jest.fn()
   let props = {
     login: {
-      id: 1
+      id: 1,
+      loggedIn: true
     }
+  }
+  let event = {
+    preventDefault: jest.fn()
   }
   beforeEach(() => {
     wrapper = shallow(<MovieCard {...props}/>);
+    instance = wrapper.instance()
   })
 
   it('should match snapshot', () => {
@@ -24,9 +29,15 @@ describe('App', () => {
 
   it('addFavorites should call addFavoriteMovie', () => {
     wrapper.setProps({addFavoriteMovie : mockFunc})
-    wrapper.instance().addFavorites()
-    console.log(wrapper.props)
+    instance.addFavorites()
     expect(mockFunc).toHaveBeenCalled()
+  })
+
+  it('should set error state', () => {
+    wrapper.setProps({login: {id:1, loggedIn: false}})
+    expect(wrapper.state('error')).toEqual('')
+    instance.toggleFavorite(event, 1)
+    expect(wrapper.state('error')).toEqual("Please login to be able to favorite a movie")
   })
 
 })
