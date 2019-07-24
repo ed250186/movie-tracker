68fcba6186ed4179b0dbc802d69652556690d20d
Nathan <nathan.froeh@gmail.com>
2019-07-23 21:13:20 -0600

    addFavoriteMovie test

--- src/Containers/MovieCard/MovieCard.test.js
+++ src/Containers/MovieCard/MovieCard.test.js
@@ -2,15 +2,31 @@ import React from 'react';
 import {shallow} from 'enzyme';
 import {MovieCard} from './MovieCard';
 
+
+//28.57 |    27.27 |     12.5 |       30
+
+//lines   ... 44,161,162,163
 describe('App', () => {
   let wrapper
-
+  let mockFunc = jest.fn()
+  let props = {
+    login: {
+      id: 1
+    }
+  }
   beforeEach(() => {
-    wrapper = shallow(<MovieCard/>);
+    wrapper = shallow(<MovieCard {...props}/>);
   })
 
   it('should match snapshot', () => {
     expect(wrapper).toMatchSnapshot()
+  })
+
+  it('addFavorites should call addFavoriteMovie', () => {
+    wrapper.setProps({addFavoriteMovie : mockFunc})
+    wrapper.instance().addFavorites()
+    console.log(wrapper.props)
+    expect(mockFunc).toHaveBeenCalled()
   })
 
 })
